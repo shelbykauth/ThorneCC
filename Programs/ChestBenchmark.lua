295f4c36f243c0421a4ba665557361804a4942fa
Shelby <shelbykauth@gmail.com>
2018-05-09 13:19:36 -0400

    Add two benchmark tests

--- Programs/ChestBenchmark.lua
+++ Programs/ChestBenchmark.lua
@@ -4,15 +4,23 @@ local tArgs = {...}
 local chestName = tArgs[1]
 
 function Wrapping (chest)
-    c = peripheral.wrap(chest)
+    local c = peripheral.wrap(chest)
+end --function
+function WrapAndGetItemMeta (chest, slot)
+    local item = peripheral.wrap(chest).getItemMeta(slot)
+end --function
+function GetItemMeta (wrapped, slot)
+    local item = wrapped.getItemMeta(slot)
 end --function
 
 local argsList = {}
 
 argsList["wrapping"] = {{chestName}}
-argsList["slots"] = {{chestName}, {}}
+argsList["nameAndSlots"] = {{chestName}, {}}
+argsList["wrapAndSlots"] = {{peripheral.wrap(chestName)}, {}}
 for i = 1,27 do
-    argsList["slots"][2][i] = i
+    argsList["nameAndSlots"][2][i] = i
+    argsList["wrapAndSlots"][2][i] = i
 end --for
 
 print(BenchmarkAPI.FormatBenchmarkFunction("Chest Peripheral Wrapping", Wrapping, argsList["wrapping"]))
