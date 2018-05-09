bd7f193ed52d225815f5228ca958dd8db982088d
Shelby <shelbykauth@gmail.com>
2018-05-09 13:09:30 -0400

    Change Parameters
    Whoops.  The function apparently can't see the information in the local
    scope.  So we're passing it in.
    
    Also, the inventory slots thing is for a different function.

--- Programs/ChestBenchmark.lua
+++ Programs/ChestBenchmark.lua
@@ -3,14 +3,16 @@ os.loadAPI("/ThorneCC/API/BenchmarkAPI.lua")
 local tArgs = {...}
 local chestName = tArgs[1]
 
-function Wrapping ()
-    c = peripheral.wrap(chestName)
+function Wrapping (chest)
+    c = peripheral.wrap(chest)
 end --function
 
 local argsList = {}
 
-argsList["wrapping"] = {{}}
+argsList["wrapping"] = {{chestName}}
+argsList["slots"] = {{chestName}, {}}
 for i = 1,27 do
-    argsList["wrapping"][1][i] = i
+    argsList["wrapping"][2][i] = i
 end --for
-BenchmarkAPI.FormatBenchmarkFunction("Chest Peripheral Wrapping", Wrapping, argsList["wrapping"])
+
+print(BenchmarkAPI.FormatBenchmarkFunction("Chest Peripheral Wrapping", Wrapping, argsList["wrapping"]))
