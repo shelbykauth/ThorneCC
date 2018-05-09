a4b7e9cbd3b26ccca3b281bb0d9141b56b24ef1f
Shelby <shelbykauth@gmail.com>
2018-05-09 13:01:19 -0400

    Actually run the function.

--- Programs/ChestBenchmark.lua
+++ Programs/ChestBenchmark.lua
@@ -6,3 +6,11 @@ local chestName = tArgs[1]
 function Wrapping ()
     c = peripheral.wrap(chestName)
 end --function
+
+local argsList = {}
+
+argsList["wrapping"] = {{}}
+for i = 1,27 do
+    argsList["wrapping"][1][i] = i
+end --for
+BenchmarkAPI.FormatBenchmarkFunction("Chest Peripheral Wrapping", Wrapping, argsList["wrapping"])
