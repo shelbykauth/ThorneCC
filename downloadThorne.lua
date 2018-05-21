e71a036b7948227c136cd4d321e15cdd97fdbf87
Shelby <shelbykauth@gmail.com>
2018-05-21 18:09:06 -0400

    Add downloadThorne program
    This is meant to be taken separately from the git repository and ran.
    It will download the necessary parts of the repository and run the
    installer.

---
+++ downloadThorne.lua
@@ -0,0 +1,8 @@
+--github implementation by max96at in 2012
+shell.run("pastebin", "get", "wPtGKMam", "github")
+
+shell.run("github", "shelbykauth", "ThorneCC", "ThorneCC/apis", "ThorneCC/apis", "master")
+shell.run("github", "shelbykauth", "ThorneCC", "ThorneCC/programs", "ThorneCC/programs", "master")
+shell.run("github", "shelbykauth", "ThorneCC", "ThorneCC/help", "help", "master")
+
+shell.run("ThorneCC/programs/Installer.lua")
